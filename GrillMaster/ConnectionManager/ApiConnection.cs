7e3225f950bcc6d29109b08ccbdc41729955bbe9
rafinha12 <rafa_j_a@hotmail.com>
2020-07-18 22:19:40 +0200

    Code added

---
+++ GrillMaster/ConnectionManager/ApiConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ConnectionManager
+{
+    public class ApiConnection
+    {
+        private string url;
+        public ApiConnection(String url)
+        {
+            this.url = url;
+        }
+
+        public HttpWebResponse Get(string urn)
+        {
+            HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(string.Concat(this.url, urn));
+            getRequest.Method = "GET";
+            getRequest.Headers.Add("Accept", "Application/json");
+            try
+            {
+                HttpWebResponse getResponse = (HttpWebResponse)getRequest.GetResponse();
+                return getResponse;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+               
+           
+            
+
+        }
+    }
+}
